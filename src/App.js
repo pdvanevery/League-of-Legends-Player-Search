851e628e5364a53e73246ffc0a566e36519efaff
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2023-02-09 10:42:07 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h5> Leage of Legends Player Search</h5>
+      </div>
+    </div>
+  );
+}
+
+export default App;
