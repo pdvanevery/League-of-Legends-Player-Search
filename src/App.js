07bcaae99882332c0753efe7cad349920fd8c3c5
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2023-02-09 10:50:55 -0800

    updates console with search text, search bar testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState("")
+  console.log(searchText)
+
   return (
     <div className="App">
       <div className="container">
         <h2> Leage of Legends Player Search</h2>
-        <input type="text"></input>
+        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
         <button>Search for Player</button>
       </div>
     </div>
